50fe6caffcb23473b8c07999ea02c880aa708c87
Diego Castillo <diegocasmo@gmail.com>
2024-06-16 16:45:56 -0500

    Updates

--- component---src-pages-404-js-176dd04922c95afb244a.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[883],{9616:function(t,e,a){a.r(e);var n=a(7294),l=a(8497),s=a(1633);e.default=function(t){var e=t.data,a=t.location,o=e.site.siteMetadata.title;return n.createElement(l.A,{location:a,title:o},n.createElement(s.p,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-176dd04922c95afb244a.js.map
