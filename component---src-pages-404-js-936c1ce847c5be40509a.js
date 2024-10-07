6bb4bff7d9fd90c020e597c1a20124f292ef182f
Diego Castillo <diegocasmo@gmail.com>
2024-10-07 14:40:35 -0500

    Updates

---
+++ component---src-pages-404-js-936c1ce847c5be40509a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[883],{9616:function(t,e,a){a.r(e);var n=a(7294),l=a(9938),s=a(1633);e.default=function(t){var e=t.data,a=t.location,o=e.site.siteMetadata.title;return n.createElement(l.A,{location:a,title:o},n.createElement(s.p,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-936c1ce847c5be40509a.js.map
